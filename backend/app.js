b441b17759a40be8f5db9d0a702eee92f8d8536b
Youngjae Lim <limyoungjae@gmail.com>
2021-09-26 19:20:40 -0400

    modified /backend/app.js to add authentication to mongodb

--- backend/app.js
+++ backend/app.js
@@ -1,101 +1,101 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require('fs')
+const path = require('path')
 
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
 
-const Goal = require('./models/goal');
+const Goal = require('./models/goal')
 
-const app = express();
+const app = express()
 
 const accessLogStream = fs.createWriteStream(
   path.join(__dirname, 'logs', 'access.log'),
-  { flags: 'a' }
-);
+  { flags: 'a' },
+)
 
-app.use(morgan('combined', { stream: accessLogStream }));
+app.use(morgan('combined', { stream: accessLogStream }))
 
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, OPTIONS')
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type')
+  next()
+})
 
 app.get('/goals', async (req, res) => {
-  console.log('TRYING TO FETCH GOALS');
+  console.log('TRYING TO FETCH GOALS')
   try {
-    const goals = await Goal.find();
+    const goals = await Goal.find()
     res.status(200).json({
       goals: goals.map((goal) => ({
         id: goal.id,
         text: goal.text,
       })),
-    });
-    console.log('FETCHED GOALS');
+    })
+    console.log('FETCHED GOALS')
   } catch (err) {
-    console.error('ERROR FETCHING GOALS');
-    console.error(err.message);
-    res.status(500).json({ message: 'Failed to load goals.' });
+    console.error('ERROR FETCHING GOALS')
+    console.error(err.message)
+    res.status(500).json({ message: 'Failed to load goals.' })
   }
-});
+})
 
 app.post('/goals', async (req, res) => {
-  console.log('TRYING TO STORE GOAL');
-  const goalText = req.body.text;
+  console.log('TRYING TO STORE GOAL')
+  const goalText = req.body.text
 
   if (!goalText || goalText.trim().length === 0) {
-    console.log('INVALID INPUT - NO TEXT');
-    return res.status(422).json({ message: 'Invalid goal text.' });
+    console.log('INVALID INPUT - NO TEXT')
+    return res.status(422).json({ message: 'Invalid goal text.' })
   }
 
   const goal = new Goal({
     text: goalText,
-  });
+  })
 
   try {
-    await goal.save();
+    await goal.save()
     res
       .status(201)
-      .json({ message: 'Goal saved', goal: { id: goal.id, text: goalText } });
-    console.log('STORED NEW GOAL');
+      .json({ message: 'Goal saved', goal: { id: goal.id, text: goalText } })
+    console.log('STORED NEW GOAL')
   } catch (err) {
-    console.error('ERROR FETCHING GOALS');
-    console.error(err.message);
-    res.status(500).json({ message: 'Failed to save goal.' });
+    console.error('ERROR FETCHING GOALS')
+    console.error(err.message)
+    res.status(500).json({ message: 'Failed to save goal.' })
   }
-});
+})
 
 app.delete('/goals/:id', async (req, res) => {
-  console.log('TRYING TO DELETE GOAL');
+  console.log('TRYING TO DELETE GOAL')
   try {
-    await Goal.deleteOne({ _id: req.params.id });
-    res.status(200).json({ message: 'Deleted goal!' });
-    console.log('DELETED GOAL');
+    await Goal.deleteOne({ _id: req.params.id })
+    res.status(200).json({ message: 'Deleted goal!' })
+    console.log('DELETED GOAL')
   } catch (err) {
-    console.error('ERROR FETCHING GOALS');
-    console.error(err.message);
-    res.status(500).json({ message: 'Failed to delete goal.' });
+    console.error('ERROR FETCHING GOALS')
+    console.error(err.message)
+    res.status(500).json({ message: 'Failed to delete goal.' })
   }
-});
+})
 
 mongoose.connect(
-  'mongodb://mongodb:27017/course-goals',
+  'mongodb://mongoadmin:secret@mongodb:27017/course-goals?authSource=admin',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   },
   (err) => {
     if (err) {
-      console.error('FAILED TO CONNECT TO MONGODB');
-      console.error(err);
+      console.error('FAILED TO CONNECT TO MONGODB')
+      console.error(err)
     } else {
-      console.log('CONNECTED TO MONGODB');
-      app.listen(80);
+      console.log('CONNECTED TO MONGODB')
+      app.listen(80)
     }
-  }
-);
+  },
+)
